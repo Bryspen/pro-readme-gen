1d90de5b14a52f72e8d0cd4c7dd755db73d52ae0
Bryan Spencer <bryanmspencer@gmail.com>
2024-08-29 17:34:28 -0500

    Created JS functions and updated function for markdown creation

--- index.js
+++ index.js
@@ -1,92 +1,85 @@
 // TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
+
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-.prompt([
-    {
-        type: 'input',
-        message: 'What is name if the project?',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'What is the description of your project?',
-        name: 'description',
-    },
-    {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: 'What is the usage information?',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'What are the credits?',
-        name: 'credits',
-    },
-    {
-        type: 'input',
-        message: 'What is the license?',
-        name: 'license',
-    },
-    {
-        type: 'input',
-        message: 'Who contributed to the project?',
-        name: 'contributing',
-    },
-    {
-        type: 'input',
-        message: 'Are there any tests to run for your project?',
-        name: 'tests',
-    }
-  ])
-  .then((response) => {
-    const md = `# ${response.name}
-    ## Description
-    ${response.description}
-    ##Table of Contents
-    - [Description](#description)
-    - [User Story](#userstory)
-    - [Acceptance Criteria](#acceptance)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [Liscense](#liscense)
-    ## installation 
-    ${response.installation}
-    ## Usage 
-    ${response.usage}
-    ## Credits
-    ${response.credits}
-    ## Liscense
-    ${response.liscense}
-    ## Contributing
-    ${response.contributing}
-    ## Tests
-    ${response.tests}`;
-    
-    fs.writeFile('utils/README.md', md, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log('utils/README.md');
-      }
-    });
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+const questions = [{
+    type: 'input', 
+    message: 'What is the title of your project?',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'What is the description of your project?',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'What are the installation instructions?',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'What is the usage information?',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'What are the credits?',
+    name: 'credits',
+},
+{
+    type: 'input',
+    message: 'How can people make contributions to the project?',
+    name: 'contributing',
+},
+{
+    type: 'input',
+    message: 'Are there any tests to run for your project?',
+    name: 'tests',
+},
+{
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'github',
+},
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+},
+{
+    type: 'list',
+    message: 'What is the license?',
+    name: 'license',
+    choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+},
+];
+
+ 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filePath, response) {
+    const data = generateMarkdown(response);
+    fs.writeFile('utils/README.md', data, (err) => {
+        if (err) {
+          console.error('Error writing to file:', err);
+        } else {
+          console.log('utils/README.md');
+        }
+      }); 
+    
+};
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        writeToFile('utils/README.md', response);
+    });
+}
 
 // Function call to initialize app
 init();
---
+++ utils/README.md
@@ -0,0 +1,28 @@
+# a
+    ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+## Description
+    a
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation 
+    a
+## Usage 
+    a
+## Credits
+  a
+## License
+  This project is licensed under the MIT license, Copyright 2024 to present.
+## Contributing
+  a
+## Tests
+  a
+## Questions
+  GitHub  [a](github.com/a)
+  Email  [a](a)
+  License  [MIT](https://opensource.org/licenses/MIT)
